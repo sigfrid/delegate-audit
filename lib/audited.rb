85b9c5509393fabcfcdc23ca2438b2becfa9c955
Sigfrid Dusci <sig@inforlife.ch>
2016-10-10 18:56:56 +0800

    WIP

--- lib/audited.rb
+++ lib/audited.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Audited < SimpleDelegator
   delegate :id, :to => :__getobj__
 
@@ -6,46 +8,75 @@ class Audited < SimpleDelegator
     @auditee_class = audited_object.class
     @audited_object = audited_object
     @auditor_class = "#{audited_object.class}Audit".constantize
-    @audited_associations = Array(options[:associations])
+    @audited_associations = through_associations_without(options[:ignore])
   end
 
-  def save
+  def save!
     ActiveRecord::Base.transaction do
       audited_changes = changes.merge(associations_changes_as_hash)
       super
       @auditor_class.create(auditee_id: id, audited_changes: audited_changes)
     end
+    self
+    rescue StandardError
+    false
   end
 
-  def revisions
+  def audits
     @auditor_class.where(auditee_id: id)
   end
 
-  private
+private
 
   def associations_changes_as_hash
     if @audited_associations.empty?
+      p "EMPTY"
       { }
     else
-      { "associations" => associations_changes.flatten }
+      #p JSON.parse(associations_changes.join(','))
+    #  p JSON.generate(associations_changes)
+      JSON.generate(associations_changes)
     end
   end
 
   def associations_changes
-    associated_collections = through_associations.map { |association| @audited_object.send(association) }
+    associated_collections = @audited_associations.map { |association| @audited_object.send(association) }
 
-    associated_collections.each_with_object([]) do |associated_collection, associations_changes|
-      associations_changes << association_changes(associated_collection)
+
+    p associated_collections
+
+     associated_collections.each_with_object({}) do |associated_collection, associations_changes|
+       p association_changes(associated_collection)
+
+
+       association_changes(associated_collection).each_with_index do |change_hash, i|
+         p change_hash
+
+        myhash = Hash[change_hash.map{|k,v| ["#{i}_#{k}",v]}]
+
+        p myhash
+        #stringHash = myhash.to_s
+        #stringJson = stringHash.gsub("=>", ":").gsub("nil", "null")
+
+      x =   associations_changes.merge(myhash)
+      p x
+      p " ----"
+      x
+
+      end
     end
   end
 
-  def through_associations
-    @audited_associations.map { |association| String(@auditee_class.reflections[association].options[:through]) }
+  def through_associations_without(ignored_associations)
+    audited_associations = Array(@auditee_class.reflect_on_all_associations(:has_many).reject { |association| Array(ignored_associations).include? association.name })
+
+    audited_associations.map { |association| association.options[:through] }
+                        .compact
   end
 
   def association_changes(associated_collection)
     Array(associated_collection).map { |association| extract_associated_object_from(association) }
-                                .reject{ |changes| changes == {} }
+                                .reject { |changes| changes == {} }
   end
 
   def extract_associated_object_from(association)
@@ -53,6 +84,6 @@ class Audited < SimpleDelegator
   end
 
   def other(association)
-   association.class.reflect_on_all_associations(:belongs_to).detect { |a| a.foreign_key !~ /@auditee_class.to_s.downcase/ and break a.foreign_key }
+   association.class.reflect_on_all_associations(:belongs_to).detect { |a| a.foreign_key !~ Regexp.new(@auditee_class.to_s.downcase) and break a.foreign_key }
   end
 end
